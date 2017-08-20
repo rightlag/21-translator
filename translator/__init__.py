8c14d4c6559cdc776a05c55097870d232a655261
rightlag <rightlag@gmail.com>
2017-08-19 22:04:19 -0400

    Add username keyword argument to Payment

--- translator/__init__.py
+++ translator/__init__.py
@@ -8,7 +8,8 @@ from two1.bitserv.flask import Payment
 application = Flask(__name__)
 payment = Payment(
     application,
-    Two1Wallet.import_from_mnemonic(mnemonic=os.getenv('TWO1_WALLET_MNEMONIC'))
+    Two1Wallet.import_from_mnemonic(mnemonic=os.getenv('TWO1_WALLET_MNEMONIC')),  # noqa: E501
+    username=os.getenv('TWO1_USERNAME')
 )
 
 import translator.views  # noqa: E402,F401
