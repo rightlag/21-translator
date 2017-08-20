d4dbc3e8a68425fb96d8f3d0148fd7b9f6ef1923
rightlag <rightlag@gmail.com>
2017-08-19 20:37:50 -0400

    Use pinned two1 module

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-        'two1',
+        'two1==3.4.1',
     ],
     author='Jason Walsh',
     url='https://github.com/rightlag/21-translator'
